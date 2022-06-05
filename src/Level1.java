e693207aba92853deb1815b2a15c465258016026
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-06-05 17:22:12 +0300

    correction of  sol number 1

--- src/Level1.java
+++ src/Level1.java
@@ -1,23 +1,19 @@
-public class Sol1 {
+import java.util.*;
+public class Level1 {
     public static int squirrel (int N) {
-        int orderNumber = 5;
-        if (N > 0) {
+        int orderNumber = 1;
+        if (N > 0) { // filter of "0"
             int factorial = N;
-            for (int i = 1; i < N; i++) {
+            for (int i = 1; i < N; i++) { //calculation of factorial
                 factorial *= i;
             }
-            char [] charFactorial = String.valueOf(factorial).toCharArray();
-            String a = "";
+            char [] charFactorial = String.valueOf(factorial).toCharArray(); // preparation for output
+            String a = ""; // the buffer
             for (int x = 0; x < (charFactorial.length - (charFactorial.length - orderNumber)); x++) {
                 a = String.valueOf(charFactorial[x]);
             }
-            return Integer.parseInt(a);
+            return Integer.parseInt(a); // output value of orderNumber
         }
-        return 1;
-
-    }
-    public static void main(String[] args) {
-        int a = 8;
-        System.out.println(squirrel(a));
+        return 1; // output factorial "0"
     }
 }
