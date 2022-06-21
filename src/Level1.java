03ae715151e89bce689b5fa911af707ecbb19901
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-06-22 01:14:49 +0300

    Solution version 5  to  task 7

--- src/Level1.java
+++ src/Level1.java
@@ -3,75 +3,84 @@ public class Level1 {
     public static int [] WordSearch (int len, String s, String subs)  {
         ArrayList<String> stringBroken = new ArrayList<>();
         StringBuilder buffer = new StringBuilder();
-        int countergap = -1; //счетчик
-        int countpoz = 0; // указатель позиции последнего пробела в заданной величине разбивки
-        int j = 0;
+        int counterpoz = 0; //указатель текущей позиции
+        int countgap = 0; // указатель позиции последнего пробела в заданной величине разбивки
         boolean flag = true;
-        int v = 0;
 
-        for ( ; j < s.length(); j++) {
-            if (countpoz == 0) {
-                countpoz = j;
-                countergap = j;
+        while (counterpoz < s.length()) {
+            if (s.charAt(counterpoz) == ' ') { //если первый символ это пробел - сдвигааем указатель позиций на 1
+                counterpoz += 1;
             }
-            if (len < (s.length() - countergap)) {
-                for (int a = 0 ; a < len; a ++) { //считаем пробелы в границах величины разбивки
-                    countergap += 1;
-                    if (s.charAt(j + a) == ' ') {
-                        countpoz = countergap;
+            //считаем позицию последнего пробела в границах разбивки
+            if (len <= (s.length() - counterpoz)) { //если величина разбивки меньше разницы длины строки и текущей позиции
+                for (int i = 0; i < len; i++) { //тогда идем на эту величину разбивки
+                    if (s.charAt(counterpoz + i) == ' ') {
+                        countgap = counterpoz + i; //посчитали позицию последнего пробела
                         flag = false;
                     }
                 }
             }
-            else {
-                v = j;
-                for ( ; v < s.length(); v ++) { //считаем пробелы в границах величины разбивки
-                    countergap += 1;
-                    if (s.charAt( v ) == ' ') {
-                        countpoz = countergap;
+            if (len >= (s.length() - counterpoz)) { //если величина разбивки  больше разницы дл строки и текущ позиции
+                for (int i = 0; i < (s.length() - counterpoz); i++) { //тогда идем на величину этой разницы
+                    if (s.charAt(counterpoz + i) == ' ') {
+                        countgap = counterpoz + i; //посчитали позицию последнего пробела
                         flag = false;
                     }
                 }
-            }
+            }//если пробел есть  флаг в фолс
             if (!flag) {
-                for ( ; j < countpoz; j++) {
-                    buffer.append(s.charAt(j));
+                for (int i = 0; i < (countgap - counterpoz); i++) { //тогда идем от указателя позиций до позиции последнего пробела
+                    buffer.append(s.charAt(counterpoz + i)); //ложим в буфер символы до последнего пробела
                 }
-                stringBroken.add(buffer.toString());
-                buffer = new StringBuilder();
-                j = countpoz - 1;
-                countpoz = 0;
-                flag = true;
+                counterpoz = countgap; // согласно указателя пробела  - выставляем указатель позицй
+                String bufferAfterTTrim = buffer.toString().trim(); //тримим буфер
+                if (bufferAfterTTrim.length() > 0) {
+                    stringBroken.add(bufferAfterTTrim); //трим строку передаем в список
+                }
+                buffer = new StringBuilder(); // обнуляем буфер
             }
-            else {
-                if ((s.length() - j) < len) {
-                    for (int n = 0; n < (s.length() - j); n++) {
-                        buffer.append(s.charAt(j + n));
+            if (flag) {    //если пробела нет  флаг в тру
+                //если величина разбивки меньше разницы длины строки и текущей позиции
+                if (len <= (s.length() - counterpoz)) {
+                    for (int i = 0; i < len; i++) { //тогда идем до величины разбивки
+                        buffer.append(s.charAt(counterpoz + i)); //ложим в буфер символы до конца строки
                     }
-                    stringBroken.add(buffer.toString());
-                    buffer = new StringBuilder();
-                    j = s.length() -1;
-                    break;
+                    counterpoz = counterpoz + len; // согласно счетчика  - выставляем указатель позицй
+                    countgap = counterpoz; // счетчик пробелов выровняли с указателем позиций
+                    String bufferAfterTrim = buffer.toString().trim(); //тримим буфер
+                    if (bufferAfterTrim.length() > 0) {
+                        stringBroken.add(bufferAfterTrim); //трим строку передаем в список
+                    }
+                    buffer = new StringBuilder(); // обнуляем буфер
                 }
-                for (int n = 0; n < len; n++) {
-                    buffer.append(s.charAt(j + n));
+                //если величина разбивки больше разницы длины строки и текущей позиции
+                if (len >= (s.length() - counterpoz)) {
+                    for (int i = 0; i < (s.length() - counterpoz); i++) { //тогда идем до конца строки
+                        buffer.append(s.charAt(counterpoz + i)); //ложим в буфер символы до концароки
+                    }
+                    counterpoz = counterpoz + (s.length() - counterpoz); // согласно счетчика  - выставляем указатель позицй
+                    countgap = counterpoz; // счетчик пробелов выровняли с указателем позиций
+                    String bufferAfterTrim = buffer.toString().trim(); //тримим буфер
+                    if (bufferAfterTrim.length() > 0) {
+                        stringBroken.add(bufferAfterTrim); //трим строку передаем в список
+                    }
+                    buffer = new StringBuilder(); // обнуляем буфер
                 }
-                stringBroken.add(buffer.toString());
-                buffer = new StringBuilder();
-                j = j + len -1;
             }
+            flag = true;
         }
 
         int [] result = new int[stringBroken.size()];
-        for (int k = 0; k < stringBroken.size(); k++) {
-            if (stringBroken.get(k).contains(subs)) {
-                result [k] = 1;
+        subs = subs.trim();
+        for ( int i = 0; i < stringBroken.size(); i++) {
+            String[] splitString = stringBroken.get(i).split(" ");
+            for (String value : splitString) {
+                if (value.equalsIgnoreCase(subs)) {
+                    result[i] = 1;
+                    break;
+                }
             }
-            else {
-                result [k] = 0;
-            }
-
         }
-            return result;
+        return result;
     }
 }
