724137686827461466784cdfe7a83c989db7a03c
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-06-21 03:24:06 +0300

    Solution version 4  to  task 7

--- src/Level1.java
+++ src/Level1.java
@@ -1,115 +1,77 @@
 import java.util.*;
 public class Level1 {
     public static int [] WordSearch (int len, String s, String subs)  {
-            ArrayList<String> stringBroken = new ArrayList<>();
-            ArrayList<String> stringBuilt = new ArrayList<>();
-            StringBuilder buffer = new StringBuilder();
-            StringBuilder bufferBuilt = new StringBuilder();
-            StringBuilder subBuffer = new StringBuilder();
-            String gap = " ";
-            int count = 0;
+        ArrayList<String> stringBroken = new ArrayList<>();
+        StringBuilder buffer = new StringBuilder();
+        int countergap = -1; //счетчик
+        int countpoz = 0; // указатель позиции последнего пробела в заданной величине разбивки
+        int j = 0;
+        boolean flag = true;
+        int v = 0;
 
-            for (int i = count; i < s.length(); i++) {
-                if (s.charAt(i) != ' ' && i+1 < s.length()) {
-                    buffer.append(String.valueOf(s.charAt(i)));
-                    continue;
+        for ( ; j < s.length(); j++) {
+            if (countpoz == 0) {
+                countpoz = j;
+                countergap = j;
+            }
+            if (len < (s.length() - countergap)) {
+                for (int a = 0 ; a < len; a ++) { //считаем пробелы в границах величины разбивки
+                    countergap += 1;
+                    if (s.charAt(j + a) == ' ') {
+                        countpoz = countergap;
+                        flag = false;
+                    }
                 }
-                if (s.charAt(i) == ' ') {
+            }
+            else {
+                v = j;
+                for ( ; v < s.length(); v ++) { //считаем пробелы в границах величины разбивки
+                    countergap += 1;
+                    if (s.charAt( v ) == ' ') {
+                        countpoz = countergap;
+                        flag = false;
+                    }
+                }
+            }
+            if (!flag) {
+                for ( ; j < countpoz; j++) {
+                    buffer.append(s.charAt(j));
+                }
+                stringBroken.add(buffer.toString());
+                buffer = new StringBuilder();
+                j = countpoz - 1;
+                countpoz = 0;
+                flag = true;
+            }
+            else {
+                if ((s.length() - j) < len) {
+                    for (int n = 0; n < (s.length() - j); n++) {
+                        buffer.append(s.charAt(j + n));
+                    }
                     stringBroken.add(buffer.toString());
                     buffer = new StringBuilder();
-                    continue;
+                    j = s.length() -1;
+                    break;
                 }
-                if (s.charAt(i) != ' ' && i == s.length()-1) {
-                    buffer.append(String.valueOf(s.charAt(i)));
-                    stringBroken.add(buffer.toString());
-                    buffer = new StringBuilder();
+                for (int n = 0; n < len; n++) {
+                    buffer.append(s.charAt(j + n));
                 }
+                stringBroken.add(buffer.toString());
+                buffer = new StringBuilder();
+                j = j + len -1;
+            }
+        }
+
+        int [] result = new int[stringBroken.size()];
+        for (int k = 0; k < stringBroken.size(); k++) {
+            if (stringBroken.get(k).contains(subs)) {
+                result [k] = 1;
+            }
+            else {
+                result [k] = 0;
             }
 
-            for (int i = 0; i < stringBroken.size(); i ++) {
-                if ( !stringBuilt.isEmpty()) {
-                    for (int f = 0; f < stringBuilt.size(); f++) {
-                        if (!stringBuilt.get(f).contains(gap) && bufferBuilt.length() == 0) {
-                            bufferBuilt = new StringBuilder();
-                            bufferBuilt.append(gap);
-                        }
-                    }
-                }
-                if ((stringBroken.get(i)).length() <= len) { //если строка меньше или равна ширине
-                    bufferBuilt.append(stringBroken.get(i));
-                    if (bufferBuilt.length() + gap.length() <= len && i != stringBroken.size() - 1) { //1*
-                        bufferBuilt.append(gap);
-                        if ( (i + 1) < stringBroken.size() && bufferBuilt.length() + (stringBroken.get(i+1)).length() <= len) {
-                            continue; //1*
-                        }
-                    }
-                    if (bufferBuilt.length() + gap.length() <= len) { //2*
-                        if ( (i + 1) < stringBroken.size() && bufferBuilt.length() + (stringBroken.get(i+1)).length() >= len) {
-                            stringBuilt.add(bufferBuilt.toString());
-                            bufferBuilt = new StringBuilder();
-                            continue; //2*
-                        }
-                    }
-                    if (bufferBuilt.length() + gap.length() >= len) { //3*
-                        stringBuilt.add(bufferBuilt.toString());
-                        bufferBuilt = new StringBuilder();
-                        continue; //3*
-                    }
-                    if (i == stringBroken.size() - 1) { //4*
-                        stringBuilt.add(bufferBuilt.toString());
-                        bufferBuilt = new StringBuilder(); //4*
-                        break;
-                    }
-                }
-
-                if ((stringBroken.get(i)).length() >= len) { // если строка больше или равна ширине
-                    bufferBuilt.append(stringBroken.get(i));
-                    for (int j = 0; j < len; j++) {
-                        subBuffer.append(bufferBuilt.charAt(j)); //забираем символы в саббуфер на величину разбивки
-                    }
-                    stringBuilt.add(subBuffer.toString());
-                    subBuffer = new StringBuilder();
-                    for (int k = len ; k < bufferBuilt.length(); k++) {
-                        subBuffer.append(bufferBuilt.charAt(k)); //забираем символы в буфер на величину разбивки
-                        if (subBuffer.length() == len && k < (bufferBuilt.length()) - 1) {
-                            stringBuilt.add(subBuffer.toString());
-                            subBuffer = new StringBuilder();
-                        }
-                    }
-                    bufferBuilt = new StringBuilder(subBuffer.toString());
-                    subBuffer = new StringBuilder();
-                }
-            }
-
-            //poiskovik
-            count = 0;
-            int b = 0; //счетчик символов s2
-            int [] result = new int[stringBuilt.size()];
-
-            for (int g = 0; g < stringBuilt.size(); g++) {
-                if (stringBuilt.get(g).length() >= subs.length()) {
-                    for (int m = 0; m < stringBuilt.get(g).length(); m++) {
-                        for (int i = 0; i < subs.length(); i++) {
-                            if (stringBuilt.get(g).charAt(m) != subs.charAt(b)) {
-                                b = 0;
-                                break;
-                            }
-                            b += 1;
-                            if (b == subs.length()) {
-                                if (m != stringBuilt.get(g).length() - 1 && stringBuilt.get(g).charAt(m) != ' ') {
-                                    b = 0;
-                                    break;
-                                }
-                                result[g] += 1;
-                                b = 0;
-                                break;
-                            }
-                            break;
-                        }
-                    }
-                }
-
-            }
+        }
             return result;
     }
 }
