ad472b61fd734e32c2b7b8fe826c215df8c35823
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-06-26 20:10:23 +0300

    Solution version 2  to  task 8

--- src/Level1.java
+++ src/Level1.java
@@ -6,39 +6,41 @@ public class Level1 {
         int sum3 = 0;
         int sum4 = 0;
 
-        for ( int i =  0; i < data.length; i++) {
+        for (int i = 0; i < N; i++) {
             if (i == 0) {
-                for (int j = (i+1); j < data.length; j++) {
-                    sum1 += data [j];
+                for (int j = (i + 1); j < N; j++) {
+                    sum1 += data[j];
                 }
-                if (data [i] == sum1) {
-                    return sum1;
+                if (data[i] == sum1) {
+                    sum4 = data[i];
                 }
                 sum1 = 0;
             }
-            if (i != data.length - 1) {
-                for (int k = i+1; k < data.length; k++) {
-                    sum2 += data [k];
+            if (i > 0 && i != N - 1) {
+                for (int k = i + 1; k < N; k++) {
+                    sum2 += data[k];
                 }
                 for (int f = 0; f < i; f++) {
                     sum3 += data[f];
                 }
-                if (data [i] == (sum2 + sum3)) {
-                    sum4 = (sum2 + sum3);
-                    return sum4;
+                if (data[i] == (sum2 + sum3)) {
+                    return data[i];
                 }
                 sum2 = 0;
                 sum3 = 0;
             }
-            if (i == data.length - 1) {
-                for (int k = 0; k < data.length - 1; k++) {
-                    sum2 += data [k];
+            if (i > 0 && i == N - 1) {
+                for (int k = 0; k < N - 1; k++) {
+                    sum2 += data[k];
                 }
-                if ( data [i] == sum2)
-                    data [i] = sum4;
-                break;
+                if (data[i] == sum2) {
+                    return data[i];
                 }
+                sum2 = 0;
             }
+
+        }
         return sum4;
     }
+
 }
