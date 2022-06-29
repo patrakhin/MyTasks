1f12d85844f92d3751c2eabf88e4856d35b25418
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-06-29 12:22:42 +0300

    Solution version 2  to  task 9

--- src/Level1.java
+++ src/Level1.java
@@ -3,11 +3,11 @@ public class Level1 {
     public static String TheRabbitsFoot (String s, boolean encode) {
         String s2 = s;
         if (encode) {
-            s2 = s.replaceAll("\\s",""); //удаляем все пробелы из строки - строка перед шифровкой
+            s2 = s.replaceAll("\\s","");
         }
-        int countgap = 0; //счетчик пробелов
-        int count = 0; // счетчик символов в строке
-        //считаем количество пробелов для дешифровки
+        int countgap = 0;
+        int count = 0;
+
         for (int i = 0; i < s2.length(); i++) {
             if (encode) {
                 break;
@@ -16,22 +16,21 @@ public class Level1 {
                 countgap += 1;
             }
         }
-        StringBuilder aftercode = new StringBuilder(); // строка после шифровки - расшифровки
-        double numderofsqrt = Math.sqrt(s2.length()); //находим кв.корень длины строки без пробелов
-        int string = (int) numderofsqrt; // строка
-        int column = (int) ((numderofsqrt - string) * 10); //столбец
-        // если количество столбцов = 0, прибавим количество посчитанных пробелов плюс 3
+        StringBuilder aftercode = new StringBuilder();
+        double numderofsqrt = Math.sqrt(s2.length());
+        int string = (int) numderofsqrt;
+        int column = (int) ((numderofsqrt - string) * 10);
+
         if (column == 0) {
             column = countgap + 3;
         }
-        //создаем матрицу
-        char [] [] redbag; // символьная матрица
+
+        char [] [] redbag;
         while ((string * column) < s2.length()) {
             string += 1;
         }
         redbag = new char[string][column];
-        //КОДИРУЕМ
-        // заполняем по строчно двумерный массив
+
         for (int i = 0; i < string; i++){
             if (!encode) {
                 break;
@@ -44,7 +43,7 @@ public class Level1 {
                 count += 1;
             }
         }
-        //выводим зашифрованный текст : сверху-вниз  справа на лево
+
         for (int j = 0; j < column; j++){
             if (!encode) {
                 break;
@@ -54,19 +53,19 @@ public class Level1 {
             }
             aftercode.append(" ");
         }
-        //РАСКОДИРУЕМ
+
         StringBuilder finish = new StringBuilder();
-        count = 0; //обнуляем счетчик
+        count = 0;
         int countStrings = 0;
         int countColum = 1;
-        //считаем количество символов в первом слове до пробела - это будет количество строк
+
         for (int j = 0 ; j < s2.length() ; j++ ) {
             if (s2.charAt(j) == ' ' || encode) {
                 break;
             }
             countStrings += 1;
         }
-        //считаем количество слов, это количество пробелов + 1
+
         for (int j = 0 ; j < s2.length() ; j++ ) {
             if (encode) {
                 break;
@@ -75,8 +74,7 @@ public class Level1 {
                 countColum += 1;
             }
         }
-        //создаем матрицу определенного нами размера
-        //заполняем по столбцам сверху вниз слева на право, если символ == “ ” при заполнении, в начале столбца, то берем следующий символ
+
         if (!encode) {
             redbag = new char[countStrings][countColum];
         }
@@ -96,7 +94,7 @@ public class Level1 {
                 count +=1;
             }
         }
-        //  создаем расшифрованную строку
+
         for (int i = 0 ; i < countStrings ; i++ ) {
             if (encode) {
                 break;
@@ -108,8 +106,7 @@ public class Level1 {
         if (!encode) {
             return finish.toString().trim();
         }
-        aftercode = new StringBuilder(aftercode.toString().replaceAll("\\p{C}", "")); //тирмим строку для выдачи результата
-        aftercode = new StringBuilder(aftercode.toString().trim());
+        aftercode = new StringBuilder(aftercode.toString().replaceAll("\\p{C}", "").trim());
         return aftercode.toString();
     }
 }
