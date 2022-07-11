2a568f1df548d0627116c76801f8748d799786f1
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-07-12 01:25:46 +0300

    Solution version 1  to  task 15

--- src/Level1.java
+++ src/Level1.java
@@ -1,89 +1,153 @@
 import java.util.*;
 public class Level1 {
-    public static int Unmanned (int L, int N, int [][] track) {
-        HashMap <String, Integer> map = new HashMap <>();
-        String [] variable = {"P","R","G"};
-        int d = track [0].length; //3
-        int tAbs = 0;
-        int diff = 0;
-        int ostatok = 0;
-        int sumtAbs = 0;
-        int buffPointTraffic = 0;
-        boolean yesStop = true;
-        boolean noStop = true;
-        boolean flag = true;
-        int theEndOfRoad = 0;
-        for (int k = 0; k < N; k++) {
-            for (int i = 0; i < d ; i++) {
-                map.put (variable[i], track [k] [i]);
-            }
-            int p = map.get("P");
-            if (p > L) {
-                theEndOfRoad = L;
-                return theEndOfRoad;
-            }
-            if (!noStop ) { // if before delay don't drop time
-                sumtAbs = 0;
-            }
-            if (!yesStop) { // if before delay drop time
-                p = (map.get("P") - buffPointTraffic) + sumtAbs + buffPointTraffic;
-            }
-            noStop = true;
-            yesStop = true;
-            tAbs = 0;
-            int r = map.get("R");
-            int g = map.get("G");
-            if (p < (r + g) && p < r && flag) {
-                tAbs =(r - p); // drop time
-                flag = false;
-            }
-            if (p < (r + g) && p >= r && flag) {
-                tAbs = 0; // it's green light and don't drop time
-                flag = false;
-            }
-            if (p == (r + g) && flag) {
-                tAbs = r; // it's red light
-                flag = false;
-            }
-            if (p > (r + g) && p % (r + g) == 0 && flag) {
-                tAbs = r; // it's red light
-                flag = false;
-            }
-            if (p > (r + g) && p % (r + g) != 0 && flag) {
-                diff = (p / (r + g));
-                ostatok = p - ((r + g) * diff); //
-            }
-            if (ostatok < (r + g) && ostatok < r && flag) {
-                tAbs =(r - ostatok); // drop time on traffic
-                flag = false;
-            }
-            if (ostatok < (r + g) && ostatok >= r && flag) {
-                tAbs = 0; // green light and time don't drop
-                flag = false;
-            }
-
-            if (tAbs == 0 && sumtAbs == 0) {  // drop time on traffic  - false and before - false
-                sumtAbs = 0; //
-                noStop = false; //
-            }
-            if (tAbs == 0 && sumtAbs != 0) {  // drop time on traffic  - false but before - true
-                buffPointTraffic = map.get("P"); // position traffic latest for synchro
-                sumtAbs += tAbs; // drop time on traffic
-                yesStop = false; //
-            }
-            if (tAbs != 0) {  // drop time on traffic  - true
-                buffPointTraffic = map.get("P"); // position traffic latest
-                sumtAbs += tAbs; // drop time on traffic
-                yesStop = false; //
-            }
-            if (k == (N -1) && sumtAbs != 0) { // this latest traffic and delay true
-                theEndOfRoad = sumtAbs + (L - (map.get("P"))) + map.get("P");
-            }
-            if (k == (N -1) && sumtAbs == 0) { // this latest traffic and there's no lag.
-                theEndOfRoad = L;
-            }
-            flag = true;
+    public static boolean TankRush (int H1, int W1, String S1, int H2, int W2, String S2) {
+        int count = 0;
+        //need vars and flags
+        int countStrings = 0; //
+        int buffSyncColumn = 0; //
+        boolean bannerSync = true; //
+        boolean bannerSync2 = true; //
+        int markBannerTrue = 0;
+        boolean bannerTrue = false; //
+        //check size
+        if (H2 > H1 || W2 > W1) {
+            return false;
         }
-        return theEndOfRoad;
+        if (H2 == 1 || W2 == 1) {
+            return false;
+        }
+        int [] [] map = new int[H1][W1];
+        int [] [] tank = new int[H2][W2];
+        //building map
+        for (int i = 0; i < H1; i++) {
+            for (int j = 0; j < W1; j++) {
+                while (count < S1.length()) {
+                    if (S1.charAt(count) == ' ') {
+                        count += 1;
+                        continue;
+                    }
+                    String d = String.valueOf(S1.charAt(count));
+                    map[i][j] = Integer.parseInt(d);
+                    count += 1;
+                    break;
+                }
+            }
+        }
+        count = 0; //
+        //building tank
+        for (int i = 0; i < H2; i++) {
+            for (int j = 0; j < W2; j++) {
+                while (count < S2.length()) {
+                    if (S2.charAt(count) == ' ') {
+                        count += 1;
+                        continue;
+                    }
+                    String v = String.valueOf(S2.charAt(count));
+                    tank[i][j] = Integer.parseInt(v);
+                    count += 1;
+                    break;
+                }
+            }
+        }
+        // searching hit
+        count = 0; // now it's hit of column
+
+        for (int i = 0; i < H2; i++) {
+            if (markBannerTrue == 1) {
+                break;
+            }
+            for (int j = 0; j < W2; j++) {
+                if (markBannerTrue == 1) {
+                    break;
+                }
+                for (int n = 0; n < H1; n ++) {
+                    if (markBannerTrue == 1) {
+                        break;
+                    }
+                    for (int m = 0; m < W1; m++) {
+                        if (!bannerSync) { //sync
+                            count = 0;
+                            m = buffSyncColumn;
+                            buffSyncColumn = 0; //
+                            bannerSync2 = false;
+                        }
+                        if (!bannerSync2 && tank [i] [0] != map [n] [m]) { //NO after sync
+                            bannerSync = true;
+                            bannerSync2 = true;
+                            count = 0;
+                            buffSyncColumn = 0;
+                            countStrings = 0;
+                            i -= 1;
+                            n -= 1;
+                            j = 0;
+                            break;
+                        }
+                        if (tank [i] [j] != map [n] [m] && m + 1 != W1 - 1) { //NO after sync will set
+                            bannerSync = true;
+                            bannerSync2 = true;
+                            count = 0;
+                            buffSyncColumn = 0;
+                            countStrings = 0;
+                            continue; // go to next numb
+                        }
+                        if (tank [i] [j] != map [n] [m] && m + 1 == W1 - 1 && tank [i] [j] != map [n] [m + 1] ) { //NO after sync will set
+                            bannerSync = true;
+                            bannerSync2 = true;
+                            count = 0;
+                            buffSyncColumn = 0;
+                            countStrings = 0;
+                            break; //  and go to next string
+                        }
+                        if (n == H1 - 1 && i != H2 - 1 && countStrings != H2) {
+                            markBannerTrue = 1;
+                            bannerTrue = false;
+                            break;
+                        }
+                        if (!bannerSync2 && tank [i] [0] == map [n] [m]) { //YES after sync
+                            bannerSync = true;
+                            bannerSync2 = true;
+                            count += 1;
+                            buffSyncColumn = m;
+                            j += 1;
+                            continue;
+                        }
+                        if (bannerSync && tank [i] [j] == map [n] [m] && count == 0 && buffSyncColumn == 0) { //YES without sync
+                            count += 1;
+                            buffSyncColumn = m; //
+                            j += 1;
+                            continue; // ИДЕМ ЗА СЛДЕДУЮЩ ЧИСЛОМ
+                        }
+                        if (bannerSync && tank [i] [j] == map [n] [m] && count != 0 && count  + 1 != W2) { //YES without sync
+                            j += 1;
+                            count += 1;
+                            continue;
+                        }
+                        if (bannerSync && tank [i] [j] == map [n] [m] && count != 0 && count  + 1 == W2) { //YES without sync
+                            count += 1;
+                        }
+                        if (bannerSync && tank [i] [j] == map [n] [m] && count == W2 && i != H2 - 1) { //YES without sync
+                            count = 0;
+                            countStrings += 1;
+                            i += 1;
+                            j = 0;
+                            bannerSync = false; // go to sync
+                            break;
+                        }
+                        if (bannerSync  && count == W2 && i == H2 - 1 && countStrings == H2 - 1 ) {
+                            countStrings = H2; //HIT!!
+                            markBannerTrue = 1;
+                            bannerTrue = true;
+                            break;
+                        }
+                        if (bannerSync  && count == W2 && i == H2 - 1 && countStrings != H2) {
+                            markBannerTrue = 1; // NO HIT !!!
+                            bannerTrue = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return bannerTrue;
     }
 }
---
+++ src/Level1_14_.java
@@ -0,0 +1,89 @@
+import java.util.*;
+public class Level1_14_ {
+    public static int Unmanned (int L, int N, int [][] track) {
+        HashMap <String, Integer> map = new HashMap <>();
+        String [] variable = {"P","R","G"};
+        int d = track [0].length; //3
+        int tAbs = 0;
+        int diff = 0;
+        int ostatok = 0;
+        int sumtAbs = 0;
+        int buffPointTraffic = 0;
+        boolean yesStop = true;
+        boolean noStop = true;
+        boolean flag = true;
+        int theEndOfRoad = 0;
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < d ; i++) {
+                map.put (variable[i], track [k] [i]);
+            }
+            int p = map.get("P");
+            if (p > L) {
+                theEndOfRoad = L;
+                return theEndOfRoad;
+            }
+            if (!noStop ) { // if before delay don't drop time
+                sumtAbs = 0;
+            }
+            if (!yesStop) { // if before delay drop time
+                p = (map.get("P") - buffPointTraffic) + sumtAbs + buffPointTraffic;
+            }
+            noStop = true;
+            yesStop = true;
+            tAbs = 0;
+            int r = map.get("R");
+            int g = map.get("G");
+            if (p < (r + g) && p < r && flag) {
+                tAbs =(r - p); // drop time
+                flag = false;
+            }
+            if (p < (r + g) && p >= r && flag) {
+                tAbs = 0; // it's green light and don't drop time
+                flag = false;
+            }
+            if (p == (r + g) && flag) {
+                tAbs = r; // it's red light
+                flag = false;
+            }
+            if (p > (r + g) && p % (r + g) == 0 && flag) {
+                tAbs = r; // it's red light
+                flag = false;
+            }
+            if (p > (r + g) && p % (r + g) != 0 && flag) {
+                diff = (p / (r + g));
+                ostatok = p - ((r + g) * diff); //
+            }
+            if (ostatok < (r + g) && ostatok < r && flag) {
+                tAbs =(r - ostatok); // drop time on traffic
+                flag = false;
+            }
+            if (ostatok < (r + g) && ostatok >= r && flag) {
+                tAbs = 0; // green light and time don't drop
+                flag = false;
+            }
+
+            if (tAbs == 0 && sumtAbs == 0) {  // drop time on traffic  - false and before - false
+                sumtAbs = 0; //
+                noStop = false; //
+            }
+            if (tAbs == 0 && sumtAbs != 0) {  // drop time on traffic  - false but before - true
+                buffPointTraffic = map.get("P"); // position traffic latest for synchro
+                sumtAbs += tAbs; // drop time on traffic
+                yesStop = false; //
+            }
+            if (tAbs != 0) {  // drop time on traffic  - true
+                buffPointTraffic = map.get("P"); // position traffic latest
+                sumtAbs += tAbs; // drop time on traffic
+                yesStop = false; //
+            }
+            if (k == (N -1) && sumtAbs != 0) { // this latest traffic and delay true
+                theEndOfRoad = sumtAbs + (L - (map.get("P"))) + map.get("P");
+            }
+            if (k == (N -1) && sumtAbs == 0) { // this latest traffic and there's no lag.
+                theEndOfRoad = L;
+            }
+            flag = true;
+        }
+        return theEndOfRoad;
+    }
+}
