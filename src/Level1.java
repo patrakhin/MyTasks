8366ad98194fc703321860da97ead00daed0257e
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-07-13 16:37:06 +0300

    Solution version 6  to  task 15

--- src/Level1.java
+++ src/Level1.java
@@ -1,13 +1,13 @@
 import java.util.*;
 public class Level1 {
     public static boolean TankRush (int H1, int W1, String S1, int H2, int W2, String S2) {
-        int count = 0;
+
         //need vars and flags
-        int countStrings = 0;
-        int buffSyncColumn = 0;
-        boolean bannerSync = true;
-        boolean bannerSync2 = true;
-        int markBannerTrue = 0;
+        int count = 0;
+
+        int countAll = 0; //
+        boolean columnFlag = true; //
+        boolean stringFlag = true; //
         boolean bannerTrue = false;
         //check size
         if (H2 > H1 || W2 > W1) {
@@ -21,137 +21,76 @@ public class Level1 {
         //building map
         for (int i = 0; i < H1; i++) {
             for (int j = 0; j < W1; j++) {
-                    if (S1.charAt(count) == ' ') {
-                        count += 1;
-                    }
-                    String d = String.valueOf(S1.charAt(count));
-                    map[i][j] = Integer.parseInt(d);
+                if (S1.charAt(count) == ' ') {
                     count += 1;
+                }
+                String d = String.valueOf(S1.charAt(count));
+                map[i][j] = Integer.parseInt(d);
+                count += 1;
             }
         }
         count = 0; //
         //building tank
         for (int i = 0; i < H2; i++) {
             for (int j = 0; j < W2; j++) {
-                    if (S2.charAt(count) == ' ') {
-                        count += 1;
-                    }
-                    String v = String.valueOf(S2.charAt(count));
-                    tank[i][j] = Integer.parseInt(v);
+                if (S2.charAt(count) == ' ') {
                     count += 1;
+                }
+                String v = String.valueOf(S2.charAt(count));
+                tank[i][j] = Integer.parseInt(v);
+                count += 1;
             }
         }
         // searching hit
-        count = 0; // now it's hit of column
-        int i = 0;
-        int j = 0;
-        int n = 0;
-        //int m = 0;
+        for (int n = 0; n < H1; n ++) {
+            for (int m = 0; m < W1; m++) {
+                for (int i = 0; i < H2; i++) {
+                    for (int j = 0; j < W2; j++) {
+                        if (m + j > W1 - 1 || m + (W2 - 1) > W1 - 1) {
+                            countAll = 0;
+                            columnFlag = false;
+                        }
+                        if (n + i > H1 - 1 || n + (H2 - 1) > H1 - 1) {
+                            countAll = 0;
+                            stringFlag = false;
+                        }
+                        if (columnFlag && stringFlag && tank [i] [j] == map [n+i] [m+j]) {
+                            countAll += 1;
+                        }
 
-        while (i < H2) {
-            if (markBannerTrue == 1) {
-                break;
-            }
-            while (j < W2) {
-                if (markBannerTrue == 1) {
-                    break;
-                }
-                while (n < H1) {
-                    if (markBannerTrue == 1) {
-                        break;
-                    }
-                    for (int m = 0; m < W1; m++) {
-                        boolean breakEr = true;
-                        boolean coninuEr = true;
-                        boolean breakSync = true;
-                        if (!bannerSync) { //sync
-                            count = 0;
-                            m = buffSyncColumn;
-                            buffSyncColumn = 0; //
-                            bannerSync2 = false;
+                        if (countAll == (W2 * H2)) {
+                            return true;
                         }
-                        if (!bannerSync2 && tank[i][0] != map[n][m]) { //NO after sync
-                            bannerSync = true;
-                            bannerSync2 = true;
-                            count = 0;
-                            buffSyncColumn = 0;
-                            countStrings = 0;
-                            i -= 1;
-                            n -= 1;
-                            j = 0;
-                            breakSync = false;
+
+                        if (columnFlag && stringFlag && countAll != 0 && countAll < (W2 * W1) && tank [i] [j] == map [n+i] [m+j]) {
+                            continue;
                         }
-                        if (tank [i] [j] != map [n] [m] && m + 1 != W1 - 1 && breakSync) { //NO after sync will set
-                            bannerSync = true;
-                            bannerSync2 = true;
-                            count = 0;
-                            buffSyncColumn = 0;
-                            countStrings = 0;
-                            m++; // go to next numb
+
+                        if (columnFlag && stringFlag && tank [i] [j] == tank [H2 - 1] [W2 - 1]  && tank [i] [j] != map [n+i] [m+j]) {
+                            countAll = 0;
+                            continue;
                         }
-                        if (tank[i][j] != map[n][m] && m + 1 == W1 - 1 && tank[i][j] != map[n][m + 1] && breakSync) { //NO after sync will set
-                            bannerSync = true;
-                            bannerSync2 = true;
-                            count = 0;
-                            buffSyncColumn = 0;
-                            countStrings = 0;
-                            breakEr = false; //  and go to next string
+                        if (columnFlag && stringFlag && tank [i] [j] != map [n+i] [m+j]) {
+                            countAll = 0;
+                            continue;
                         }
-                        if (n == H1 - 1 && i != H2 - 1 && countStrings != H2 && breakEr && breakSync) {
-                            markBannerTrue = 1;
-                            breakEr = false;
-                        }
-                        if (!bannerSync2 && tank[i][0] == map[n][m] && breakSync) { //YES after sync
-                            bannerSync = true;
-                            bannerSync2 = true;
-                            count += 1;
-                            buffSyncColumn = m;
-                            j += 1;
-                            coninuEr = false;
-                        }
-                        if (bannerSync && tank [i] [j] == map [n] [m] && count == 0 && buffSyncColumn == 0 && coninuEr && breakSync) { //YES without sync
-                            count += 1;
-                            buffSyncColumn = m; //
-                            j += 1;
-                            coninuEr = false; // go to next number
-                        }
-                        if (bannerSync && tank [i] [j] == map [n] [m] && count != 0 && count  + 1 != W2 && coninuEr && breakSync) { //YES without sync
-                            j += 1;
-                            count += 1;
-                        }
-                        if (bannerSync && tank [i] [j] == map [n] [m] && count != 0 && count  + 1 == W2 && breakSync) { //YES without sync
-                            count += 1;
-                        }
-                        if (bannerSync && tank [i] [j] == map [n] [m] && count == W2 && i != H2 - 1 && breakEr && breakSync) { //YES without sync
-                            count = 0;
-                            countStrings += 1;
-                            i += 1;
-                            j = 0;
-                            bannerSync = false; // go to sync
-                            breakEr = false;
-                        }
-                        if (bannerSync  && count == W2 && i == H2 - 1 && countStrings == H2 - 1 && breakEr && breakSync) {
-                            countStrings = H2; //HIT!!
-                            markBannerTrue = 1;
-                            bannerTrue = true;
-                            breakEr = false;
-                        }
-                        if (bannerSync  && count == W2 && i == H2 - 1 && countStrings != H2 && breakEr && breakSync) {
-                            markBannerTrue = 1; // NO HIT !!!
-                            breakEr = false;
-                        }
-                        if (!breakEr || !breakSync) {
+                        if (!columnFlag && stringFlag) {
+                            columnFlag = true;
+                            m = W1;
+                            i = H2;
+                            j = W2;
                             break;
                         }
-
+                        if (columnFlag && !stringFlag) {
+                            return false;
+                        }
+                        if (!columnFlag && !stringFlag) {
+                            return false;
+                        }
                     }
-                    n++;
                 }
-                j++;
             }
-            i++;
         }
         return bannerTrue;
     }
-
 }
