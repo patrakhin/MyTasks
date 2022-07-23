3f1d0a4db7a6a09cb29c28d26f211eb6706cca85
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-07-24 01:47:40 +0300

    add solution ver 1 task 20

--- src/Level1.java
+++ src/Level1.java
@@ -1,114 +1,273 @@
 import java.util.*;
 public class Level1 {
-    public static String [] ShopOLAP(int N, String [] items) {
-        HashMap<String, Integer> mapItems = new HashMap<>();
-        HashMap <Integer, String> mapIndex = new HashMap<>(); //for double sel of item
-        boolean breakEr = false;
-        String key = ""; //id-Items
-        String value = ""; //id-Sel
-        boolean flagFull = false;
-        String [] stringKey = new String[items.length];
-        int [] intValue = new int[items.length];
-        //if keys equal - addition
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < items[i].length(); j++) {
-                if (items[i].charAt(j) != ' ' && !breakEr) {
-                    key += items[i].charAt(j);
-                }
-                if (items[i].charAt(j) == ' ') {
-                    breakEr = true;
-                }
-                if (items[i].charAt(j) != ' ' && breakEr) {
-                    value += items[i].charAt(j);
-                }
-            }
-            if (mapItems.containsKey(key)) {
-                mapItems.put(key, (mapItems.get(key) + Integer.parseInt(value)));
-            }
-            if (!mapItems.containsKey(key)) {
-                mapItems.put(key, Integer.parseInt(value));
-            }
-            stringKey [i] = key;
-            intValue [i] = Integer.parseInt(value);
+    //input string
+    static String commandNow;
+    //current string
+    static  String currentString = "";
+    //Array - storage
+    static ArrayList<String> arrayStorage = new ArrayList<>();
+    //count array storage
+    static int countArrayStorage = 0;
+    //Array - saving all command
+    static ArrayList<String> arraySavingAllCommand = new ArrayList<>();
+    //Array saving Put and Delete
+    static  ArrayList <String> savingPutAndDelete = new ArrayList<>();
+    //Array store Delete
+    static ArrayList <String> storeDelete = new ArrayList<>();
+    //Count saving Put and Del
+    static int countSavingPutAndDelete = 0;
+    //Count for store Delete
+    static int countStoreDelete = 0;
+    // Array store Put
+    static ArrayList <String> storePut = new ArrayList<>();
+    // count array store put
+    static int countStorePut = 0;
+    //Array store for escape will Undo its use in Redo
+    static ArrayList <String> storeForRedo = new ArrayList<>();
+    //count storeRedo
+    static int countStoreRedo = 0;
+    // Array store DeleteItems for Redo
+    static ArrayList <String> storeDeleteItemsForRedo = new ArrayList<>();
+    //count store DeleteItems for Redo
+    static  int countStoreDeleteItemsForRedo = 0;
+    public static String BastShoe(String command) {
 
-            key = "";
-            value = "";
-            breakEr = false;
+        commandNow += command;
+
+        // my code
+
+        HashMap <Integer, String> mapCommand = new HashMap<>();
+        //creat map - command
+        mapCommand.put(1, "Put");
+        mapCommand.put(2, "DeleteItem");
+        mapCommand.put(3, "OutputItem");
+        mapCommand.put(4, "Undo");
+        mapCommand.put(5, "Redo");
+        //
+        ArrayList<String> arrayStorage = new ArrayList<>();
+
+        //command buffer
+        String commandBuffer = "";
+        // strings buffer
+        StringBuilder stringBuffer = new StringBuilder();
+        //flag commandBuffer - is empty
+        boolean flagCommandBuffer = false;
+        // buffer for items delete
+        String bufferDelete = "";
+        // number of items for delete
+        int itemsDelete =0;
+        //flag for running Delete
+        boolean runningDelete = false;
+        //flag for OutputItem
+        boolean flagOutputItem = false;
+        //buffer for get arrayStorage string
+        String getArrayStorageString = "";
+        // number of items for OutputItem
+        int itemsOutput =0;
+        // OutputItem string
+        String stringOutputItem = "";
+        //flag saving command Undo()
+        boolean flagSavingUndo = false;
+        //Buffer string for store Delete
+        String bufSoreDelete = "";
+        // command buffer for Undo
+        String commandBufUndo = "";
+        //command buffer for Redo
+        String commandBufferRedo = "";
+
+
+
+
+
+        //loop - reader incoming string
+        for (int i = 0; i < command.length(); i++) {
+
+            if (i == 0 && mapCommand.containsKey(Integer.parseInt(String.valueOf(command.charAt(i))))) {
+                commandBuffer = mapCommand.get(Integer.parseInt(String.valueOf(command.charAt(i))));
+                continue;
+            }
+            if (i == 0 && !mapCommand.containsKey(Integer.parseInt(String.valueOf(command.charAt(i))))) {
+                System.out.println(currentString);
+            }
+            if (commandBuffer.contains("Undo") && command.length() > 1) {  // condition for UNDO
+                System.out.println(currentString);
+            }
+            if (commandBuffer.contains("Redo") && command.length() > 1) {  // condition for REDO
+                System.out.println(currentString);
+            }
+            if (i == 1 && command.charAt(i) == ' ') {
+                continue;
+            }
+
+            stringBuffer.append(command.charAt(i));
+        }
+        //block saving all command
+        if (commandBuffer.length() > 0) {
+            arraySavingAllCommand.add(commandBuffer);
+        }
+        if (commandBuffer.equals("Undo")) {
+            flagSavingUndo = true;
+        }
+        //block saving Put and Delete
+        if (commandBuffer.equals("Put")) {
+            savingPutAndDelete.add(commandBuffer);
+            countSavingPutAndDelete += 1;
+        }
+        if (commandBuffer.equals("DeleteItem")) {
+            savingPutAndDelete.add(commandBuffer);
+            countSavingPutAndDelete += 1;
+        }
+        //block saving store Delete
+        for (int i = 0; i < arrayStorage.size() && commandBuffer.equals("DeleteItem"); i++) {
+            bufSoreDelete += arrayStorage.get(i);
+        }
+        if (bufSoreDelete.length() > 0) {
+            storeDelete.add(bufSoreDelete);
+            countStoreDelete += 1;
+        }
+        bufferDelete = "";
+        //block command-arrayStorage with Put
+        if (commandBuffer.equals("Put")) {
+            arrayStorage.add(String.valueOf(stringBuffer));
+            countArrayStorage += 1;
+            // add saving to array store put
+            storePut.add(String.valueOf(stringBuffer));
+            countStorePut += 1;
+            stringBuffer = new StringBuilder("");
+        }
+        //block output string with Put
+        if (commandBuffer.equals("Put")) {
+            currentString = "";
+        }
+        for (int i = 0; i < arrayStorage.size(); i++) {
+
+            if (commandBuffer.equals("Put")) {
+                currentString += arrayStorage.get(i);
+            }
+        }
+        if (commandBuffer.equals("Put")) {
+            commandBuffer = "";
+            flagCommandBuffer = true; // command buffer is empty!!!
+        }
+        //block command-arrayStorage with DeleteItem
+        if (commandBuffer.equals("DeleteItem") && Integer.parseInt(String.valueOf(stringBuffer)) <= (String.valueOf(arrayStorage.get(arrayStorage.size() - 1))).length()) {
+            itemsDelete = Integer.parseInt(String.valueOf(stringBuffer));
+            stringBuffer = new StringBuilder(String.valueOf(arrayStorage.get(arrayStorage.size() - 1)));
+            arrayStorage.remove(arrayStorage.size() - 1);
+            runningDelete = true;
         }
 
-        //refresh stringKey after build mapItems
-        ArrayList <String> newStringKey = new ArrayList<>(mapItems.keySet());
-        stringKey = newStringKey.toArray(new String[0]);
-
-
-        //refresh intValue after build mapItems
-        intValue = new int[mapItems.size()];
-        ArrayList <Integer> intValueRefresh = new ArrayList<>(mapItems.values());
-        for (int i = 0; i < intValueRefresh.size(); i++) {
-            intValue [i] = intValueRefresh.get(i);
-        }
-
-
-        //put in mapIndex double values
-        for (int i = 0; i < mapItems.size(); i++) {
-            for (int k = i + 1; k < mapItems.size(); k++) {
-                if (intValue[i] == intValue[k]) {
-                    mapIndex.put(i, stringKey[i]);
-                    mapIndex.put(k, stringKey[k]);
-                }
+        for ( int i = 0 ;i < (stringBuffer.length() - 1) - (itemsDelete - 1) ; i++) {
+            if (runningDelete) {
+                bufferDelete += stringBuffer.charAt(i);
             }
         }
-        // if mapIndex don't empty - flag = true
-        if (mapIndex.size() != 0) {
-            flagFull = true;
+        if (runningDelete) {
+            arrayStorage.add(bufferDelete);
+
         }
-        //sort double values to lexicography
-        List<String> listLexicography = new ArrayList<>(mapIndex.values());
-        listLexicography.sort(null);
-        //revers selItems
-        HashMap <Integer, String> selItems = new HashMap<>();
-        for (int i = 0; i < mapItems.size(); i++) {
-            selItems.put(intValue[i], stringKey [i]);
+        //block output string with DELETE
+        if (commandBuffer.equals("DeleteItem")) {
+            currentString = "";
         }
-        //sort intValue
-        for (int i = 0; i < intValue.length; i++) {
-            for (int j = i+1; j < intValue.length; j++) {
-                if (intValue[i] < intValue[j]) {
-                    int buff = intValue [i];
-                    intValue [i] = intValue[j];
-                    intValue [j] = buff;
-                }
+        for (int i = 0; i < arrayStorage.size(); i++) {
+            if (commandBuffer.equals("DeleteItem")) {
+                currentString += arrayStorage.get(i);
             }
         }
-        //endArray
-        boolean bulidDouble = false;
-        boolean flagEnd = false;
-        String [] endArray = new String[mapItems.size()];
-        for (int i = 0; i < mapItems.size(); i++) {
-            for (String s : listLexicography) {
-                if (i == mapItems.size() - 1 && intValue[i - 1] == intValue[i]) {
-                    endArray[i] = (s + ' ' + mapItems.get(s));
-                    flagEnd = true;
-                    break;
-                }
-                if (flagFull && intValue[i] == intValue[i + 1] && i + 1 != mapItems.size()) {
-                    endArray[i] = (s + ' ' + mapItems.get(s));
-                    i += 1;
-                    bulidDouble = true;
-                    continue;
-                }
-                if (flagFull && intValue[i] != intValue[i + 1] && i + 1 != mapItems.size() && bulidDouble) {
-                    endArray[i] = (s + ' ' + mapItems.get(s));
-                    i += 1;
-                    break;
-                }
-            }
-            if (flagEnd) {
+        if (commandBuffer.equals("DeleteItem")) {
+            itemsDelete = 0;
+            stringBuffer = new StringBuilder("");
+            bufferDelete = "";
+            runningDelete = false; // bufferDelete is empty!!!
+            commandBuffer = "";
+            flagCommandBuffer = true; // command buffer is empty!!!
+        }
+        //block output string with OutputItem
+        if (commandBuffer.equals("OutputItem")) {
+            itemsOutput = Integer.parseInt(String.valueOf(stringBuffer));
+            flagOutputItem = true;
+            stringBuffer = new StringBuilder("");
+        }
+        for (int i = 0; i < arrayStorage.size() && flagOutputItem; i++) {
+            getArrayStorageString += String.valueOf(arrayStorage.get(i));
+        }
+        for (int i = 0; i < getArrayStorageString.length(); i++) {
+            if (flagOutputItem && itemsOutput <= getArrayStorageString.length() - 1) {
+                stringOutputItem = String.valueOf(getArrayStorageString.charAt(itemsOutput));
+                currentString = stringOutputItem; // здесь не обнулил строку т.к. это конечный вывод
+                //обнули буфе ры что в этом блоке ЗДЕСЬЬ!!!
+                itemsOutput = 0;
+                flagOutputItem = false;
+                getArrayStorageString = "";
+                stringBuffer = new StringBuilder("");
                 break;
             }
-            endArray [i] = (selItems.get(intValue[i]) + ' '+ mapItems.get(selItems.get(intValue[i])));
         }
-        return endArray;
+        if (flagOutputItem && itemsOutput > stringBuffer.length() - 1) {
+            currentString = "";
+            //обнули буфе ры что в этом блоке ЗДЕСЬЬ ТОЖЕ т.к. это другая ветка события!!!
+            itemsOutput = 0;
+            flagOutputItem = false;
+            getArrayStorageString = "";
+            stringBuffer = new StringBuilder("");
+        }
+        //block Undo()
+
+        if (commandBuffer.equals("Undo") && countStoreDelete > 0) {
+            System.out.println(savingPutAndDelete);
+            commandBufUndo = savingPutAndDelete.get(countSavingPutAndDelete -1);
+            countSavingPutAndDelete -= 1;
+        }
+        // if in block Undo contains Put
+        if (commandBufUndo.contains("Put")) {
+            //add in store Redo
+            storeForRedo.add(arrayStorage.get(arrayStorage.size() - 1));
+            countStoreRedo += 1;
+            arrayStorage.remove(arrayStorage.size() - 1); //удаляем из Арр СТОРЕ послед ячейку, а если надо будет ее вернуть? Когда буд делать РеДо предусм буфер для удаленных
+            currentString = "";
+        }
+        for (int i = 0; i < arrayStorage.size() && commandBufUndo.contains("Put"); i++) {
+            currentString += arrayStorage.get(i);
+        }
+        if (commandBufUndo.contains("Put")) {
+            commandBufUndo = "";
+        }
+        // if in block Undo contains DeleteItems
+        if (commandBufUndo.contains("DeleteItem")) {
+            //add in store Redo
+            storeForRedo.add(arrayStorage.get(arrayStorage.size() - 1));
+            countStoreRedo += 1;
+            currentString = storeDelete.get(countStoreDelete - 1);
+            countStoreDelete -= 1;
+        }
+        //block Redo
+        if (commandBuffer.equals("Redo") && countStorePut > 0 && countStoreRedo > 0) {
+            System.out.println(savingPutAndDelete);
+            System.out.println(storeForRedo);
+            System.out.println(savingPutAndDelete.get(storeForRedo.size() -1));
+            commandBufferRedo = savingPutAndDelete.get(storeForRedo.size() -1);
+        }
+        if (commandBufferRedo.contains("Put")) {
+            currentString = "";
+            //currentString = String.valueOf(storeForRedo);
+            System.out.println(arrayStorage);
+            countStorePut -= 1;
+            countStoreRedo -= 1;
+        }
+        for (int i = 0; i < arrayStorage.size() && commandBufferRedo.contains("Put"); i++) {
+            currentString += arrayStorage.get(i);
+        }
+        if (commandBufferRedo.contains("DeleteItem")) {
+            arrayStorage.add(storeDelete.get(countStoreDelete -1));
+            countStoreDelete -= 1;
+            countStoreRedo -= 1;
+        }
+        for (int i = 0; i < arrayStorage.size() && commandBufferRedo.contains("DeleteItem"); i++) {
+            currentString += arrayStorage.get(i);
+        }
+        commandBufferRedo = "";
+        return currentString;
     }
 
 }
