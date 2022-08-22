e258aa0ca2bba2c988dab8d8eed6028e7a696788
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-08-22 15:04:26 +0300

    add test for check 0 task 20

--- src/Level1Test.java
+++ src/Level1Test.java
@@ -34,39 +34,5 @@ class Level1Test {
 
         String actual_7_1 = Level1.BastShoe("3-");
         assertEquals(expected_2, actual_7_1);
-
-        //excepted item
-        String actual_8_1 = Level1.BastShoe("3 0");
-        String expected_8 = "P";
-        assertEquals(expected_8, actual_8_1);
-
-        Level1.BastShoe("1 rivet");
-        String actual_9_1 = Level1.BastShoe("3 1");
-        String expected_9 = "r";
-        assertEquals(expected_9, actual_9_1);
-
-        Level1.BastShoe("4");
-        String actual_10_1 = Level1.BastShoe("3 2");
-        String expected_10 = "i";
-        assertEquals(expected_10, actual_10_1);
-
-        Level1.BastShoe("4");
-        Level1.BastShoe("5");
-        String actual_11_1 = Level1.BastShoe("3 3");
-        String expected_11 = "v";
-        assertEquals(expected_11, actual_11_1);
-
-        Level1.BastShoe("4");
-        Level1.BastShoe("5");
-        String actual_12_1 = Level1.BastShoe("3 4");
-        String expected_12 = "e";
-        assertEquals(expected_12, actual_12_1);
-
-        Level1.BastShoe("4");
-        Level1.BastShoe("5");
-        String actual_13_1 = Level1.BastShoe("3 5");
-        String expected_13 = "t";
-        assertEquals(expected_13, actual_13_1);
     }
-
 }
