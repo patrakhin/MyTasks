7fbf266093cbd876f77dfe56b3ab23442a4ff43a
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-08-23 01:00:15 +0300

    add check 3  for task 20

--- src/TestCaseThree.java
+++ src/TestCaseThree.java
@@ -1,14 +1,30 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestThree {
-
+    @BeforeAll
+    static void bastShoe_3 () {
+        String start = "1 Privet";
+        Level1.BastShoe(start);
+    }
     @Test
     void bastShoe() {
-        //excepted item
-        Level1.BastShoe("1 Privet");
-        String actual = Level1.BastShoe("3 2");
-        String expected = "i";
+        String actual = Level1.BastShoe("3 0");
+        String expected = "P";
         assertEquals(expected, actual);
+
+        String actual_1 = Level1.BastShoe("2 2");
+        String expected_1 = "Priv";
+        assertEquals(expected_1, actual_1);
+
+        String actual_2 = Level1.BastShoe("4");
+        String expected_2 = "Privet";
+        assertEquals(expected_2, actual_2);
+
+        String actual_3 = Level1.BastShoe("5");
+        String expected_3 = "Priv";
+        assertEquals(expected_3, actual_3);
+
     }
 }
