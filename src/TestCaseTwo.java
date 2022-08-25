659a1851d6527696f1a5a76f3266429575e2b551
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-08-25 11:37:44 +0300

    add check 2  for task 20

--- src/TestCaseTwo.java
+++ src/TestCaseTwo.java
@@ -2,13 +2,63 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestCaseTwo {
-
     @Test
     void bastShoe() {
-        Level1.BastShoe("1 Privet");
-        Level1.BastShoe("3 0");
-        String actual = Level1.BastShoe("1 ut");
-        String expected = "Put";
+        String actual = Level1.BastShoe("1 Privet");
+        String expected = "Privet";
         assertEquals(expected, actual);
+
+        String actual_0 = Level1.BastShoe("1  Mir");
+        String expected_0 = "Privet Mir";
+        assertEquals(expected_0, actual_0);
+
+        String actual_0_1 = Level1.BastShoe("1 ++");
+        String expected_0_1 = "Privet Mir++";
+        assertEquals(expected_0_1, actual_0_1);
+
+        String actual_1_0 = Level1.BastShoe("3 7");
+        String expected_1_0 = "M";
+        assertEquals(expected_1_0, actual_1_0);
+
+        String actual_1 = Level1.BastShoe("3 8");
+        String expected_1 = "i";
+        assertEquals(expected_1, actual_1);
+
+        String actual_1_1 = Level1.BastShoe("3 9");
+        String expected_1_1 = "r";
+        assertEquals(expected_1_1, actual_1_1);
+
+        String actual_1_3_1_1 = Level1.BastShoe("1  Privet");
+        String expected_1_3_1_1 = "Privet Mir++r Privet";
+        assertEquals(expected_1_3_1_1, actual_1_3_1_1);
+
+        String actual_2 = Level1.BastShoe("4");
+        String expected_2 = "Privet Mir++";
+        assertEquals(expected_2, actual_2);
+
+        String actual_2_0 = Level1.BastShoe("5");
+        String expected_2_0 = "Privet Mir++r Privet";
+        assertEquals(expected_2_0, actual_2_0);
+
+        String actual_2_1 = Level1.BastShoe("4");
+        String expected_2_1 = "Privet Mir++";
+        assertEquals(expected_2_1, actual_2_1);
+
+        String actual_2_1_1 = Level1.BastShoe("1 )"); // Reload !!!
+        String expected_2_1_1 = "Privet Mir++)";
+        assertEquals(expected_2_1_1, actual_2_1_1);
+
+        String actual_3 = Level1.BastShoe("1 A");
+        String expected_3 = "Privet Mir++)A";
+        assertEquals(expected_3, actual_3);
+
+        String actual_3_1 = Level1.BastShoe("3 7");
+        String expected_3_1 = "M";
+        assertEquals(expected_3_1, actual_3_1);
+
+        String actual_3_1_1 = Level1.BastShoe("4");
+        String expected_3_1_1 = "Privet Mir++)M";
+        assertEquals(expected_3_1_1, actual_3_1_1);
+
     }
 }
