10a19e513085742a92952767f0de7d001c7e114f
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-08-26 20:27:18 +0300

    add check 4  for task 20

---
+++ src/test_4.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class test_4 {
+    @Test
+    void bastShoe() {
+        //test forever Undo
+        String actual = Level1.BastShoe("1 a");
+        String expected = "a";
+        assertEquals(expected, actual);
+
+        String actual_1 = Level1.BastShoe("1 b");
+        String expected_1 = "ab";
+        assertEquals(expected_1, actual_1);
+
+        String actual_5 = Level1.BastShoe("4");
+        String expected_5 = "a";
+        assertEquals(expected_5, actual_5);
+
+        String actual_6 = Level1.BastShoe("4");
+        String expected_6 = "";
+        assertEquals(expected_6, actual_6);
+
+        String actual_7 = Level1.BastShoe("4");
+        String expected_7 = "";
+        assertEquals(expected_7, actual_7);
+
+        String actual_8 = Level1.BastShoe("4");
+        String expected_8 = "";
+        assertEquals(expected_8, actual_8);
+    }
+}
