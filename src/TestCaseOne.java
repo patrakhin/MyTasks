af360b265aff6a6e81d94d71cce66a48e3e09b90
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-08-26 18:08:28 +0300

    add check TestOne  for task 20

--- src/TestCaseOne.java
+++ src/TestCaseOne.java
@@ -44,6 +44,10 @@ class TestCaseOne {
         String expected_9 = "";
         assertEquals(expected_9, actual_9);
 
+        String actual_9_1 = Level1.BastShoe("4");
+        String expected_9_1 = "";
+        assertEquals(expected_9_1, actual_9_1);
+
         String actual_10 = Level1.BastShoe("5");
         String expected_10 = "a";
         assertEquals(expected_10, actual_10);
@@ -52,72 +56,43 @@ class TestCaseOne {
         String expected_11 = "ab";
         assertEquals(expected_11, actual_11);
 
+        //it's block Put+Output  and Undo
         String actual_12 = Level1.BastShoe("1 A");
         String expected_12 = "abA";
         assertEquals(expected_12, actual_12);
-
-        String actual_13 = Level1.BastShoe("4");
-        String expected_13 = "ab";
-        assertEquals(expected_13, actual_13);
-
-        String actual_14 = Level1.BastShoe("1 B");
-        String expected_14 = "abB";
-        assertEquals(expected_14, actual_14);
-
-        //break Undo story
 
         String actual_15 = Level1.BastShoe("3 0");
         String expected_15 = "a";
         assertEquals(expected_15, actual_15);
 
-        String actual_16 = Level1.BastShoe("3 1");
-        String expected_16 = "b";
+        String actual_16 = Level1.BastShoe("4");
+        String expected_16 = "abcde";
         assertEquals(expected_16, actual_16);
 
         String actual_17 = Level1.BastShoe("4");
-        String expected_17 = "abb";
+        String expected_17 = "abcd";
         assertEquals(expected_17, actual_17);
 
+        // break story Undo
+
         String actual_18 = Level1.BastShoe("1 D");
-        String expected_18 = "abbD";
+        String expected_18 = "abcdD";
         assertEquals(expected_18, actual_18);
 
         String actual_19 = Level1.BastShoe("2 2");
-        String expected_19 = "ab";
+        String expected_19 = "abc";
         assertEquals(expected_19, actual_19);
 
-        String actual_20 = Level1.BastShoe("3 0");
-        String expected_20 = "a";
+        String actual_20 = Level1.BastShoe("4");
+        String expected_20 = "abcdD";
         assertEquals(expected_20, actual_20);
 
         String actual_21 = Level1.BastShoe("4");
-        String expected_21 = "abbDa";
+        String expected_21 = "abcd";
         assertEquals(expected_21, actual_21);
 
         String actual_22 = Level1.BastShoe("1 R");
-        String expected_22 = "abbDaR";
+        String expected_22 = "abcdR";
         assertEquals(expected_22, actual_22);
-
-        //break Undo story
-
-        String actual_23 = Level1.BastShoe("5");
-        String expected_23 = "abbDaR";
-        assertEquals(expected_23, actual_23);
-
-        String actual_24 = Level1.BastShoe("4");
-        String expected_24 = "abbDa";
-        assertEquals(expected_24, actual_24);
-
-        String actual_25 = Level1.BastShoe("1 P");
-        String expected_25 = "abbDaP";
-        assertEquals(expected_25, actual_25);
-
-        String actual_26 = Level1.BastShoe("3 3");
-        String expected_26 = "D";
-        assertEquals(expected_26, actual_26);
-
-        String actual_27 = Level1.BastShoe("4");
-        String expected_27 = "abbDaD";
-        assertEquals(expected_27, actual_27);
     }
 }
