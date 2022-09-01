de3d277445cfed4f68e570acd67ab52db5d7dffe
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-09-01 18:34:41 +0300

    add sol 2  Three of Life  for task 23

--- src/Level1.java
+++ src/Level1.java
@@ -2,7 +2,6 @@ import java.util.*;
 public class Level1 {
     public static String [] TreeOfLife (int H, int W, int N, String [] tree) {
         int [] [] digitalTree = new int[H][W];
-
         //recoding
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < W; j++) {
@@ -16,69 +15,66 @@ public class Level1 {
             }
         }
         //Tree
-        boolean flagGrowUp = false;
-        boolean flagGrowDelete;
-
+        boolean flagGrowDelete = false;
+        int countAges = 0;
+        int ageDelete = 3;
         for (int i = 1; i < N + 1; i++) {
-            if (i % 2 == 0) {
+            countAges += 1;
+            if (countAges % 2 == 0) {
                 flagGrowDelete = true;
             }
-            else {
-                flagGrowUp = true;
-                flagGrowDelete = false;
-            }
             // ears 1 3 5 ... only grow up
-            for (int a = 0; a < H && flagGrowUp; a++) {
+            for (int a = 0; a < H ; a++) {
                 for (int j = 0; j < W; j++) {
                     digitalTree [a] [j] += 1;
                 }
             }
-            flagGrowUp = false;
             // ears 2 4 6 ... grow up and delete
             for (int x = 0; x < 1 && flagGrowDelete; x++) {
-                // grow up
+                // delete Horizontal
                 for (int a = 0; a < H; a++) {
                     for (int j = 0; j < W; j++) {
-                        digitalTree [a] [j] += 1;
-                    }
-                }
-                // delete Horizontal if branch 3 5 or 5< age
-                for (int a = 0; a < H; a++) {
-                    for (int j = 0; j < W; j++) {
-                        int ageBranch = digitalTree [a] [j];
-                        if (j == 0 && (ageBranch == 3 || ageBranch > 3)) {
-                            digitalTree[a][j + 1] = 0;
+                        if ((j == W - 1) && (digitalTree[a][j] < ageDelete) && (digitalTree[a][j -1] >= ageDelete)) {
+                            digitalTree[a][j] = 0;
                         }
-                        if (j != 0 && j != (digitalTree[a].length - 1) && (ageBranch == 3 || ageBranch > 3)) {
+                        if ((j == W - 1) && (digitalTree[a][j] >= ageDelete) && (digitalTree[a][j -1] < ageDelete)) {
                             digitalTree[a][j - 1] = 0;
-                            digitalTree[a][j + 1] = 0;
                         }
-                        if (j == (digitalTree[a].length - 1) && (ageBranch == 3 || ageBranch > 3)) {
+                        if ((j > 0) && (j != W - 1) && (digitalTree[a][j] >= ageDelete) && (digitalTree[a][j -1] < ageDelete)) {
                             digitalTree[a][j - 1] = 0;
+                        }
+                        if ((j > 0) && (j != W - 1) && (digitalTree[a][j] < ageDelete) && (digitalTree[a][j -1] >= ageDelete) && (digitalTree[a][j +  1] < ageDelete)) {
+                            digitalTree[a][j] = 0;
                         }
                     }
                 }
-                // delete Vertical if branch 3 5 or 5< age
+                // delete Vertical
                 for (int j = 0; j < W; j ++) {
                     for (int a = 0; a < H; a ++) {
-                        int ageBranch = digitalTree [a] [j];
-                        if (a == 0 && (ageBranch == 3 || ageBranch > 3)) {
+                        if ((a == H - 1) && (digitalTree[a][j] < ageDelete) && (digitalTree[a - 1][j] >= ageDelete)) {
                             digitalTree[a][j] = 0;
-                            digitalTree[a + 1][j] = 0;
                         }
-                        if (a != 0 && a != (digitalTree.length - 1) && (ageBranch == 3 || ageBranch > 3)) {
-                            digitalTree[a][j] = 0;
+                        if ((a == H - 1) && (digitalTree[a][j] >= ageDelete) && (digitalTree[a - 1][j] < ageDelete)) {
                             digitalTree[a - 1][j] = 0;
-                            digitalTree[a + 1][j] = 0;
                         }
-                        if (a == (digitalTree.length - 1) && (ageBranch == 3 || ageBranch > 3)) {
-                            digitalTree[a][j] = 0;
+                        if ((a > 0) && (a != H - 1) && (digitalTree[a][j] >= ageDelete) && (digitalTree[a - 1][j] < ageDelete)) {
                             digitalTree[a - 1][j] = 0;
+                        }
+                        if ((a > 0) && (a != H - 1) && (digitalTree[a][j] < ageDelete) && (digitalTree[a - 1][j] >= ageDelete) && (digitalTree[a + 1][j] < ageDelete)) {
+                            digitalTree[a][j] = 0;
                         }
                     }
                 }
-
+                // delete only old branch
+                for (int j = 0; j < W; j ++) {
+                    for (int a = 0; a < H; a ++) {
+                        if ((digitalTree[a][j] >= ageDelete)) {
+                            digitalTree[a][j] = 0;
+                        }
+                    }
+                }
             }
+            flagGrowDelete = false;
         }
         //coding
         String[] exit = new String[tree.length];
