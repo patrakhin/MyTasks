70654bcec119e39f151959e860e5d396c41df717
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-09-04 13:40:38 +0300

    add Test 2  Matrix spinning  for task 24

--- src/TestMatrixSpin.java
+++ src/TestMatrixSpin.java
@@ -6,41 +6,34 @@ class TestMatrixSpin {
 
     @org.junit.jupiter.api.Test
     void matrixTurn1 () {
-        Level1 level1 = new Level1();
+
         String [] Matrix = new String[] {"12", "43"};
-        level1.MatrixTurn(Matrix, 2, 2, 1);
-        String [] actual= level1.getMatrix();
+        Level1.MatrixTurn(Matrix, 2, 2, 1);
         String [] expected = {"41", "32"};
-        assertArrayEquals(expected, actual);
+        assertArrayEquals(expected, Matrix);
     }
 
     @org.junit.jupiter.api.Test
     void matrixTurn2 () {
-        Level1 level1 = new Level1();
         String [] Matrix = new String[] {"12345", "23456", "34567", "45678"};
-        level1.MatrixTurn(Matrix, 4, 5, 1);
-        String [] actual= level1.getMatrix();
+        Level1.MatrixTurn(Matrix, 4, 5, 1);
         String [] expected = {"21234", "34345", "45656", "56787"};
-        assertArrayEquals(expected, actual);
+        assertArrayEquals(expected, Matrix);
     }
 
     @org.junit.jupiter.api.Test
     void matrixTurn3 () {
-        Level1 level1 = new Level1();
         String [] Matrix = new String[] {"1234", "2345", "3456", "4567", "5678"};
-        level1.MatrixTurn(Matrix, 5, 4, 1);
-        String [] actual= level1.getMatrix();
+        Level1.MatrixTurn(Matrix, 5, 4, 1);
         String [] expected = {"2123", "3434", "4545", "5656", "6787"};
-        assertArrayEquals(expected, actual);
+        assertArrayEquals(expected, Matrix);
     }
 
     @org.junit.jupiter.api.Test
     void matrixTurn4 () {
-        Level1 level1 = new Level1();
         String [] Matrix = new String[] {"123456", "234567", "345678", "456789"};
-        level1.MatrixTurn(Matrix, 4, 6, 1);
-        String [] actual= level1.getMatrix();
+        Level1.MatrixTurn(Matrix, 4, 6, 1);
         String [] expected = {"212345", "343456", "456767", "567898"};
-        assertArrayEquals(expected, actual);
+        assertArrayEquals(expected, Matrix);
     }
 }
