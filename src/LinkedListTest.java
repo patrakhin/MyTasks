758659f1f03664c6e59a2967b1a99e014d1868b1
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-09-13 22:45:09 +0300

    add Test to sol 4  LinkedList and delete import  for task 1

---
+++ src/LinkedListTest.java
@@ -0,0 +1,147 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+    @org.junit.jupiter.api.Test
+    void addInTail1 () { //
+        int a = 128;
+        int b = 128;
+        int c = 128;
+        int d = 128;
+        int e = 128;
+        int f = 128;
+        int g = 12;
+        LinkedList s_list = new LinkedList();
+        s_list.addInTail(new Node(a));
+        s_list.addInTail(new Node(b));
+        s_list.addInTail(new Node(c));
+        s_list.addInTail(new Node(d));
+        s_list.addInTail(new Node(e));
+        s_list.addInTail(new Node(f));
+        s_list.addInTail(new Node(g));
+        s_list.removeAll(12);
+        String expected = "[128, 128, 128, 128, 128, 128]";
+        assertEquals(expected, s_list.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInTail2 () { //
+        int a = 128;
+        int b = 128;
+        int c = 128;
+        int d = 12;
+        int e = 12;
+        int f = 12;
+        LinkedList s_list = new LinkedList();
+        s_list.addInTail(new Node(a));
+        s_list.addInTail(new Node(b));
+        s_list.addInTail(new Node(c));
+        s_list.addInTail(new Node(d));
+        s_list.addInTail(new Node(e));
+        s_list.addInTail(new Node(f));
+        s_list.removeAll(12);
+        String expected = "[128, 128, 128]";
+        assertEquals(expected, s_list.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInTail3 () { //
+        int a = 128;
+        int b = 128;
+        int c = 128;
+        int d = 128;
+        int e = 128;
+        int f = 12;
+        LinkedList s_list = new LinkedList();
+        s_list.addInTail(new Node(a));
+        s_list.addInTail(new Node(b));
+        s_list.addInTail(new Node(c));
+        s_list.addInTail(new Node(d));
+        s_list.addInTail(new Node(e));
+        s_list.addInTail(new Node(f));
+        s_list.removeAll(12);
+        String expected = "[128, 128, 128, 128, 128]";
+        assertEquals(expected, s_list.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInTail4 () { //
+        int a = 12;
+        int b = 128;
+        int c = 128;
+        int d = 128;
+        int e = 128;
+        int f = 128;
+        LinkedList s_list = new LinkedList();
+        s_list.addInTail(new Node(a));
+        s_list.addInTail(new Node(b));
+        s_list.addInTail(new Node(c));
+        s_list.addInTail(new Node(d));
+        s_list.addInTail(new Node(e));
+        s_list.addInTail(new Node(f));
+        s_list.removeAll(12);
+        String expected = "[128, 128, 128, 128, 128]";
+        assertEquals(expected, s_list.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInTail5 () { //
+        int a = 12;
+        int b = 12;
+        int c = 128;
+        int d = 12;
+        int e = 128;
+        int f = 128;
+        LinkedList s_list = new LinkedList();
+        s_list.addInTail(new Node(a));
+        s_list.addInTail(new Node(b));
+        s_list.addInTail(new Node(c));
+        s_list.addInTail(new Node(d));
+        s_list.addInTail(new Node(e));
+        s_list.addInTail(new Node(f));
+        s_list.removeAll(12);
+        String expected = "[128, 128, 128]";
+        assertEquals(expected, s_list.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInTail6 () { //
+        int a = 128;
+        int b = 128;
+        int c = 128;
+        int d = 12;
+        int e = 128;
+        int f = 128;
+        LinkedList s_list = new LinkedList();
+        s_list.addInTail(new Node(a));
+        s_list.addInTail(new Node(b));
+        s_list.addInTail(new Node(c));
+        s_list.addInTail(new Node(d));
+        s_list.addInTail(new Node(e));
+        s_list.addInTail(new Node(f));
+        s_list.removeAll(12);
+        String expected = "[128, 128, 128, 128, 128]";
+        assertEquals(expected, s_list.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInTail7 () { //
+        int a = 128;
+        int b = 128;
+        int c = 128;
+        int d = 128;
+        int e = 128;
+        int f = 128;
+        LinkedList s_list = new LinkedList();
+        s_list.addInTail(new Node(a));
+        s_list.addInTail(new Node(b));
+        s_list.addInTail(new Node(c));
+        s_list.addInTail(new Node(d));
+        s_list.addInTail(new Node(e));
+        s_list.addInTail(new Node(f));
+        s_list.removeAll(12);
+        String expected = "[128, 128, 128, 128, 128, 128]";
+        assertEquals(expected, s_list.toString());
+    }
+}
