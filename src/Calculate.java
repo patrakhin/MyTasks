2db189b0a1e0aa746483732a6b85b7ef105d1918
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-09-24 14:48:50 +0300

    add sol 4  for task Stack

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,9 +1,12 @@
 import java.util.*;
 public class Calculate {
 
-    public static int calculate (String sol) {
+    public static int calculating (String sol) {
         Stack<String> stack1 = new Stack<>();
         Stack<Integer> stack2 = new Stack<>();
+        int val1 = 0;
+        int val2 = 0;
+        int result;
         for (int i = 0; i < sol.length(); i++) {
             String temp = String.valueOf(sol.charAt(i));
             if (temp.equals(" ")) {
@@ -16,30 +19,33 @@ public class Calculate {
             if (Character.isDigit(buffer.charAt(0))) {
                 stack2.push(Integer.parseInt(buffer));
             }
+            if (buffer.contains("=")) {
+                break;
+            }
+            if (!Character.isDigit(buffer.charAt(0))) {
+                val1 = stack2.pop();
+                val2 = stack2.pop();
+            }
             if (buffer.contains("+")) {
-                int result = stack2.pop() + stack2.pop();
+                result = val1 + val2;
                 stack2.push(result);
             }
             if (buffer.contains("-")) {
-                int val1 = stack2.pop();
-                int val2 = stack2.pop();
-                int result = val1 - val2;
+                result = val1 - val2;
                 stack2.push(result);
             }
             if (buffer.contains("*")) {
-                int result = stack2.pop() * stack2.pop();
+                result = val1 * val2;
                 stack2.push(result);
             }
             if (buffer.contains("/")) {
-                int val1 = stack2.pop();
-                int val2 = stack2.pop();
-                int result = val1 / val2;
+                result = val1 / val2;
                 stack2.push(result);
             }
         }
         return stack2.pop();
     }
     public static void main(String[] args) {
-        System.out.println(calculate("= + 9 * 5 + 2 8"));
+        System.out.println(calculating("= + 9 * 5 + 2 8"));
     }
 }
