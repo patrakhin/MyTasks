711dc982c16048b6601c10d8366251b741a6c22b
Andrey Patrakhin <patrakhin2010@gmail.com>
2022-09-27 16:53:17 +0300

    add sol 2  for task DequeTest

--- src/DequeTest.java
+++ src/DequeTest.java
@@ -5,104 +5,76 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DequeTest <T> {
 
     @org.junit.jupiter.api.Test
-    void addFront() {
+    void addFront() { //list addFront/remTail
         Deque deque = new Deque<>();
         deque.addFront(1);
         deque.addFront(2);
         deque.addFront(3);
-        assertEquals(3, deque.size());
-        assertEquals(3, deque.removeFront());
         assertEquals(1, deque.removeTail());
-        deque.addTail(7);
-        assertEquals(7, deque.removeFront());
-        assertEquals(1, deque.size());
-        assertEquals(2,deque.removeFront());
-        assertNull(deque.removeTail());
-        assertNull(deque.removeFront());
-
+        assertEquals(2, deque.removeTail());
+        assertEquals(3, deque.removeTail());
+        assertEquals(0, deque.onePart.count); // count list
+        assertEquals(0, deque.secondPart.count); // count array
+        assertEquals(0, deque.size());
     }
 
     @org.junit.jupiter.api.Test
-    void addTail() {  // add front and remove front
+    void addFront1() { //list addFront/remTail
         Deque deque = new Deque<>();
         deque.addFront(1);
         deque.addFront(2);
         deque.addFront(3);
-        assertEquals(3, deque.removeFront());
-        assertEquals(2, deque.removeFront());
-        assertEquals(1, deque.removeFront());
+        assertEquals(1, deque.removeTail());
+        assertEquals(2, deque.removeTail());
+        deque.addFront(4);
+        assertEquals(4,(int) deque.onePart.tail.value);
+        assertEquals(3,(int) deque.onePart.head.value);
+        deque.addFront(5);
+        assertEquals(3, deque.removeTail());
+        // as stack
+        assertEquals(5, deque.removeFront());
+        assertEquals(4, deque.removeFront());
+
     }
 
     @org.junit.jupiter.api.Test
-    void addTail2() {  //
+    void addFront2() { //list addTail/remFront
         Deque deque = new Deque<>();
         deque.addTail(1);
         deque.addTail(2);
         deque.addTail(3);
-        assertEquals(1, deque.removeTail());
-        assertEquals(2, deque.removeTail());
-        assertEquals(3, deque.removeTail());
-    }
-
-
-    @org.junit.jupiter.api.Test
-    void removeFront() { // add front and remove tail
-        Deque deque = new Deque<>();
-        deque.addFront(1);
-        deque.addFront(2);
-        deque.addFront(3);
-        assertEquals(1, deque.removeTail());
-        assertEquals(2, deque.removeTail());
-        deque.addFront(5);
-        deque.addFront(4);
-        assertEquals(3, deque.removeTail());
-    }
-
-    @org.junit.jupiter.api.Test
-    void removeTail() {
-        Deque deque = new Deque<>();
-        deque.addFront(1);
-        deque.addFront(2);
-        deque.addFront(3);
-        assertEquals(1, deque.removeTail());
-        assertEquals(2, deque.removeTail());
-        deque.addFront(5);
-        deque.addFront(4);
-        assertEquals(3, deque.removeTail());
-    }
-
-    @org.junit.jupiter.api.Test
-    void removeTail2() {
-        Deque deque = new Deque<>();
-        deque.addFront(1);
-        deque.addFront(2);
-        deque.addFront(3);
-        deque.addFront(4);
-        deque.addFront(5);
-        deque.addFront(6);
-        deque.addFront(7);
-        deque.addFront(8);
+        assertEquals(1, deque.removeFront());
+        assertEquals(2, deque.removeFront());
+        deque.addTail(4);
         deque.addFront(9);
-        deque.addFront(10);
-        deque.addFront(11);
-        deque.addFront(12);
-        deque.addFront(13);
-        deque.addFront(14);
-        deque.addFront(15);
-        deque.addFront(16);
-        deque.addFront(17);
-        assertEquals(1, deque.removeTail());
-        assertEquals(17, (int) deque.onePart.head.value);
-        assertEquals(2, (int)  deque.onePart.tail.value);
-        assertNull(deque.onePart.tail.next);
-        assertNull(deque.onePart.head.prev);
-        assertEquals(2, deque.removeTail());
-        deque.addFront(100);
-        deque.addFront(200);
-        assertEquals(3, deque.removeTail());
+        assertEquals(9, deque.removeFront());
+        assertEquals(3, deque.removeFront());
+        assertEquals(4, deque.removeFront());
     }
 
     @org.junit.jupiter.api.Test
-    void size() {
+    void addFront3() { //list addTail/remFront
+        Deque deque = new Deque<>();
+        deque.addTail(1);
+        deque.addTail(2);
+        deque.addTail(3);
+        deque.addFront(9);
+        deque.addFront(8);
+        deque.addFront(7);
+        assertEquals(3, deque.removeTail());
+        assertEquals(2, deque.removeTail());
+        deque.addFront(4);
+        assertEquals(4,(int) deque.onePart.tail.value);
+        assertEquals(1,(int) deque.onePart.head.value);
+        deque.addFront(5);
+        assertEquals(1, deque.removeTail());
+        // as stack
+        assertEquals(5, deque.removeFront());
+        assertEquals(4, deque.removeFront());
+        assertEquals(3, deque.onePart.count); // count list
+        assertEquals(3, deque.secondPart.count); // count array
+
     }
+
 }
+
